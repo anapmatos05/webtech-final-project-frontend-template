8b80381ba3fbc55bcabdfa6c6e225f9b72962301
EricQSC <anacmmanulc@gmail.com>
2026-06-22 11:26:40 +0100

    resolução de conexão

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterModule } from '@angular/router';
-import { AuthService } from './services/auth.service'; // <-- Importamos o nosso serviço
+import { CommonModule } from '@angular/common'; // <-- Importaste bem aqui!
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterModule], 
-  templateUrl: './app.component.html', // Mantemos o teu HTML!
+  // 1. ADICIONADO AQUI O CommonModule:
+  imports: [RouterOutlet, RouterModule, CommonModule], 
+  templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'CineTrack Frontend'; 
 
-  // Injetamos o serviço para o podermos usar
-  constructor(private authService: AuthService) {}
+  // 2. MUDADO de 'private' para 'public' para o HTML conseguir ler:
+  constructor(public authService: AuthService) {}
 
   // A nossa função de teste
   testarLigacao() {
